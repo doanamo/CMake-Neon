9c428aecfda7598c68add318c02f0c1beaeee9aa
Piotr Doan <doanpiotr@gmail.com>
2022-09-04 03:16:40 +0200

    Move logger severity level defines

--- Source/Precompiled.hpp
+++ Source/Precompiled.hpp
@@ -17,12 +17,6 @@ COMPILER_WARNINGS_DISABLE_ALL_PUSH()
 #include <fmt/format.h>
 
 // External/spdlog
-#ifdef NDEBUG
-    #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_INFO
-#else
-    #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
-#endif
-
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
